92693be26e3af5bab1eb3032f1b3f1c502374f08
Ruslanios22 <ilgamovich@gmail.com>
2023-08-04 12:14:49 +0300

    Added Profile Model

---
+++ MiniCV/MiniCV/Models/Profile.swift
@@ -0,0 +1,24 @@
+//
+//  Profile.swift
+//  MiniCV
+//
+//  Created by Руслан Шигапов on 04.08.2023.
+//
+
+struct Profile {
+    let photo: String
+    let fullName: String
+    let info: String
+    let location: String
+    let about: String
+    
+    static func getProfile() -> Profile {
+        Profile(
+            photo: "Photo",
+            fullName: "Шигапов Руслан Ильгамович",
+            info: "Junior iOS-разработчик, опыт более 1-го года",
+            location: "Казань",
+            about: "Желаю развиваться как инженер в команде единомышленников, которые ставят целью разработку сверхкачественного продукта, участвовать в решении сложных и интересных задач. Меня мотивирует и заряжает технологический прогресс, быть в тренде передовых технологий и применять их в проектах."
+        )
+    }
+}
